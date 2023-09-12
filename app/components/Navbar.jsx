6b5ccb1a6ac79513ff4fe4c948bfc9a966423656
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-09-12 06:14:19 +0300

    styles, fonts, images

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
+import NavLogo from "./dojo-logo.png";
 
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src={NavLogo}
+        alt="Dojo Helpdesk Logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,39 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h2>Dashboard</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, debitis
+        inventore perspiciatis autem eius nostrum eaque.
+      </p>
+
+      <div className="flex justify-center my-8">
+        <Link href="/tickets">
+          <button className="btn-primary">View Tickets</button>
+        </Link>
+      </div>
+
+      <h2>Company Updates</h2>
+      <div className="card">
+        <h3>New member of the web dev team...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex nobis,
+          corrupti ipsum accusamus explicabo dicta veniam blanditiis possimus
+          rem soluta modi tempora dolorem dolorum libero sint harum dignissimos,
+          qui architecto eos cum!
+        </p>
+      </div>
+      <div className="card">
+        <h3>New website live!</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia,
+          quam quia ipsa animi voluptas nisi amet facilis pariatur optio
+          deleniti quos inventore nulla accusantium reprehenderit cupiditate
+          consequatur numquam!
+        </p>
+      </div>
     </main>
   );
 }
